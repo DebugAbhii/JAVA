80f683b89c31d42e8ee805ee4bca6b90ffd9e641
DebugAbhii <praviinrajput24@gmail.com>
2025-06-02 16:23:51 +0530

    Update SingleInheritance.java

--- SingleInheritance.java
+++ SingleInheritance.java
@@ -1,1 +1,21 @@
+class boy{
+    void play(){
+        System.out.println("Boy is Eating");
+    }
+}
 
+class Himesh extends boy{
+    void rest(){
+        System.out.println("Resting");
+    }
+}
+ //--------ABHIJEET VERMA 2302220100006---------\\
+
+public class SingleInheritance{                   
+    public static void main(String[] args){       
+        Himesh H = new Himesh();
+
+        H.play();
+        H.rest();
+    }
+}
